0f93fc85313e84412bfbfa7321273d62df2582c7
XinrongHe <48892130+XinrongHe@users.noreply.github.com>
2019-04-15 17:10:01 -0700

    Add a button instead of refreshing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+function play(){
+
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
@@ -12,4 +14,6 @@ if(randomNumber1>randomNumber2){
   document.querySelector("div h1").textContent = "Player 2 Wins!";
 }else{
   document.querySelector("div h1").textContent = "Draw!";
+}
+
 }
